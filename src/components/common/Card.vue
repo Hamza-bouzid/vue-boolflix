be0042abc326ee49fe489ac1b3bcbe65e369c8d1
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-02-03 20:17:12 +0100

    Milestone 4 Header non fatto

--- src/components/common/Card.vue
+++ src/components/common/Card.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="card">
-    <ul>
-      <div class="card-image">
-        <img v-if="card.poster_path" :src="image + card.poster_path" />
-        <img v-else-if="card.backdrop_path" :src="image + card.backdrop_path" />
-        <img v-else-if="card.profile_path" :src="image + card.profile_path" />
-        <img v-else src="../../assets/img/no-picture.webp" />
-      </div>
-
-      <li>{{ card.title }} {{ card.name }}</li>
-
-      <li v-if="card.original_title || card.original_name">{{ card.original_title }} {{ card.original_name }}</li>
-
-      <li v-if="card.original_language == 'it'"><img :src="flag + 'it.png'" /></li>
-      <li v-else-if="card.original_language == 'en'"><img :src="flag + 'gb.png'" /></li>
-      <li v-else><img :src="flag + 'aq.png'" /></li>
-
-      <li>
-        {{ arrotondareNumero(card.vote_average) }}
-        <div class="stars"><div class="star-inner" :class="starsWidth(arrotondareNumero(card.vote_average))"></div></div>
-      </li>
-    </ul>
+    <div class="card-front">
+      <img v-if="card.poster_path" :src="image + card.poster_path" />
+      <img v-else-if="card.backdrop_path" :src="image + card.backdrop_path" />
+      <img v-else-if="card.profile_path" :src="image + card.profile_path" />
+      <img v-else src="../../assets/img/no-picture.webp" />
+    </div>
+    <div class="card-back">
+      <ul>
+        <li><span>Titolo:</span> {{ card.title }} {{ card.name }}</li>
+        <li v-if="card.original_title || card.original_name"><span>Titolo originale:</span>{{ card.original_title }} {{ card.original_name }}</li>
+        <li v-if="card.original_language == 'it'"><span>Lingua:</span><img :src="flag + 'it.png'" /></li>
+        <li v-else-if="card.original_language == 'en'"><span>Lingua:</span><img :src="flag + 'gb.png'" /></li>
+        <li v-else><span>Lingua:</span><img :src="flag + 'aq.png'" /></li>
+        <li>
+          <!--{{ arrotondareNumero(card.vote_average) }}-->
+          <span>Voto:</span>
+          <div class="stars"><div class="star-inner" :class="starsWidth(arrotondareNumero(card.vote_average))"></div></div>
+        </li>
+      </ul>
+      <div class="overview-text"><span>Overview:</span>{{ card.overview }}</div>
+    </div>
   </div>
 </template>
 
@@ -56,12 +56,12 @@ li {
 }
 .card {
   width: 200px;
-  height: 200px;
-  margin: 100px 10px;
+  height: 300px;
+  margin: 30px 20px;
+  cursor: pointer;
+  position: relative;
 
-  &-image {
-    width: 100%;
-    height: 300px;
+  &-front {
     img {
       width: 100%;
       height: 100%;
@@ -69,56 +69,99 @@ li {
     }
   }
 
+  &-front,
+  &-back {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    backface-visibility: hidden;
+    position: absolute;
+    transition: all 0.6s linear;
+    border-radius: 10px;
+  }
+
+  &-front {
+    transform: perspective(600px) rotateY(0deg);
+  }
+
+  &-back {
+    padding: 10px;
+    background-color: rgba(0, 0, 0, 0.9);
+    transform: perspective(600px) rotateY(180deg);
+  }
+
+  &:hover > .card-front {
+    transform: perspective(600px) rotateY(-180deg);
+  }
+  &:hover > .card-back {
+    transform: perspective(600px) rotateY(0deg);
+  }
+
+  span {
+    font-weight: bold;
+    margin-right: 5px;
+    color: #fff;
+    font-size: 15px;
+  }
+
   li {
+    margin: 5px 0px;
     img {
       width: 20px;
     }
   }
+}
 
-  .stars {
-    position: relative;
-    display: inline-block;
+.overview-text {
+  max-height: 150px;
+  font-size: 12px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
 
-    &::before {
-      content: "\f005 \f005 \f005 \f005 \f005";
-      font-family: "Font Awesome 5 Free";
-      font-weight: 900;
-      color: rgba(204, 204, 204, 0.507);
-    }
+.stars {
+  position: relative;
+  display: inline-block;
+
+  &::before {
+    content: "\f005 \f005 \f005 \f005 \f005";
+    font-family: "Font Awesome 5 Free";
+    font-weight: 900;
+    color: rgba(204, 204, 204, 0.507);
   }
+}
 
-  .star-inner {
-    position: absolute;
-    top: 0;
-    left: 0;
-    white-space: nowrap;
-    overflow: hidden;
+.star-inner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  white-space: nowrap;
+  overflow: hidden;
 
-    &::before {
-      content: "\f005 \f005 \f005 \f005 \f005";
-      font-family: "Font Awesome 5 Free";
-      font-weight: 900;
-      color: #f8ce0b;
-    }
+  &::before {
+    content: "\f005 \f005 \f005 \f005 \f005";
+    font-family: "Font Awesome 5 Free";
+    font-weight: 900;
+    color: #f8ce0b;
   }
 }
 
 .starFill0 {
-    width: 0%;
-  }
-  .starFill1 {
-    width: 20%;
-  }
-  .starFill2 {
-    width: 40%;
-  }
-  .starFill3 {
-    width: 60%;
-  }
-  .starFill4 {
-    width: 80%;
-  }
-  .starFill5 {
-    width: 100%;
-  }
+  width: 0%;
+}
+.starFill1 {
+  width: 20%;
+}
+.starFill2 {
+  width: 40%;
+}
+.starFill3 {
+  width: 60%;
+}
+.starFill4 {
+  width: 80%;
+}
+.starFill5 {
+  width: 100%;
+}
 </style>
